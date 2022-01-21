a060ea739dc5395ce2765af212a129458b389651
Shigni <Louisservant86@gmail.com>
2022-01-21 12:06:18 +0100

    Close modal function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,22 @@ function editNav() {
   }
 }
 
-// DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll('.close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// launch modal
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal
+function closeModal() {
+  modalbg.style.display = 'none';
+}
+closeBtn[0].addEventListener('click', closeModal);
 
