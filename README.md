c954ee097c14c6912625ac9feb6c90795eba10c7
Shigni <Louisservant86@gmail.com>
2022-01-28 14:22:09 +0100

    reorganize

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
-# Projet GameOn
-1. Forkez ce repo ;
-2. Il est conseillé d'utiliser VisualStudio Code et vous pouvez utiliser Docker, mais ce n'est pas obligatoire ;
-3. Il n'y a aucune dépendance ;
-4. Vous ne devez utiliser que du CSS personnalisé et du JavaScript pur, sans jQuery, Bootstrap ou autre librairie.
+
+# 4/ Créez une landing page avec JavaScript
+
+- Travailler sur le repo GitHub forké
+- Programmer en JavaScript.
+- Tester manuellement les fonctionnalités, les entrées de formulaire et l'affichage responsive.
+- Utiliser des fichiers séparés pour le HTML, le CSS et le JavaScript.
+
+
+## Demo
+
+ https://shigni.github.io/LouisServant_4_21012022/
+
+
+## Authors
+
+- [@Shigni](https://www.github.com/shigni)
+
+
+## 🔗 Links
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/louis-servant-0985761ba/)
+
+
--- starterOnly/Js/modal.js
+++ starterOnly/Js/modal.js
@@ -6,23 +6,34 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelectorAll('.close');
-
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 // launch modal
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 // Close modal
 function closeModal() {
   modalbg.style.display = 'none';
 }
 closeBtn[0].addEventListener('click', closeModal);
+
+const modalSubmit = document.getElementsByClassName('container-confirmation-submit');
+const closeModalSubmit = document.getElementsByClassName('close-modal-submit');
+const closeBtnConfirmation = document.getElementById('close-btn-confirmation');
+const modalForm = document.getElementById('form');
+
+// Display confirmation
+function displayModalSubmit() {
+    const modalBody = document.querySelector('.modal-body')
+    modalForm.style.display = 'none';
+    let div = document.createElement('div')
+    div.className = ('modal-confirmation')
+    div.innerHTML = '<p>Nous avons bien pris en compte votre inscription<br><br>à bientôt !</p>';
+    modalBody.appendChild(div);
+}
 
